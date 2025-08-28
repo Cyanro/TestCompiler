5facf60479424ede41b64e6acb30fa5c0118c7d1
Cyanro <113330384+Cyanro@users.noreply.github.com>
2025-08-28 10:15:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,12 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.28/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.1.28/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.28/g' package/base-files/luci2/bin/config_generate
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
 # Modify hostname
-sed -i 's/LEDE/CyanTec/g' package/base-files/files/bin/config_generate
+# sed -i 's/LEDE/CyanTec/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/CyanTec/g' package/base-files/luci2/bin/config_generate
